37c89cfdd9d2ccfe3b0b337b9228b25f5cfe3306
Eduardo Eisman <eeisman@riplife.es>
2016-09-06 10:02:11 +0200

    .

--- fingerprint2.js
+++ fingerprint2.js
@@ -723,7 +723,7 @@
       ctx.arc(75, 75, 25, 0, Math.PI * 2, true);
       ctx.fill("evenodd");
 
-      document.body.appendChild(canvas);
+      document.getElementById('canvasContainer').appendChild(canvas);
 
       result.push("canvas fp:" + canvas.toDataURL());
       return result.join("~");
